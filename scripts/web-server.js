23b13daecdee321dd822179b1a5a80d6ed110470
Efrin González <efringonzalez@gmail.com>
2018-01-04 17:01:00 +0100

    server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -5,16 +5,16 @@ var app = express();
 var rootPath = path.normalize(__dirname + '/../');//root of app
 var bodyParser = require('body-parser');
 
-/*app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static( rootPath + '/app'));
 
 app.get('/data/event/:id', events.get);
 app.get('/data/event', events.getAll);
 app.post('/data/event/:id', events.save);
-app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });*/
+app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });
 
-app.use(express.static('/app'));
+//app.use(express.static('/app'));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
