d5e747744558501b8e5680350fc54b788c451c62
Efrin González <efringonzalez@gmail.com>
2018-01-04 14:25:55 +0100

    hello world express

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -8,11 +8,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static( rootPath + '/app'));
-
+/*
 app.get('/data/event/:id', events.get);
 app.get('/data/event', events.getAll);
 app.post('/data/event/:id', events.save);
-app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });
-
+app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });*/
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 app.listen(8000);
 console.log('Listening on port ' + 8000 + '...');
